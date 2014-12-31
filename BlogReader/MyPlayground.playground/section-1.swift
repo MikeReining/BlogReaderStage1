1934a3d8ff96a15944f40105435f3c71ffbcedd3
Mike Reining <emailmike@gmail.com>
2014-12-30 16:40:01 -0800

    Team Treehouse - Blog Reader App in Swift Stage 1

---
+++ BlogReader/MyPlayground.playground/section-1.swift
@@ -0,0 +1,20 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var objects = [String]()
+objects = [
+    "Getting started with WordPress",
+    "Whitespace in Web Design: What It Is and Why You Should Use It",
+    "Adaptive Images and Responsive SVGs - Treehouse Show Episode 15",
+    "Productivity is About Constraints and Concentration",
+    "A Guide to Becoming the Smartest Developer on the Planet",
+    "Teacher Spotlight: Zac Gordon",
+    "Do You Love What You Do?",
+    "Applying Normalize.css Reset - Quick Tip",
+    "How I Wrote a Book in 3 Days",
+    "Responsive Techniques, JavaScript MVC Frameworks, Firefox 16 | Treehouse Show Episode 14"
+]
+objects
+objects.count
+
